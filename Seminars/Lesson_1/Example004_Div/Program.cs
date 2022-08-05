03f581657c593a671ce4355ba6e4657bd6193de1
Bogdan <pozdnyakov.bogdan00@gmail.com>
2022-08-05 07:40:34 +0600

    Make Exercise 4. Programm Div. Folder Example004_Div.

---
+++ Seminars/Lesson_1/Example004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿double NumberA = new Random().Next(1, 10);
+double NumberB = new Random().Next(1, 10);
+Console.WriteLine("First number: " + NumberA);
+Console.WriteLine("Second number: " + NumberB);
+double result = NumberA / NumberB;
+Console.WriteLine("Result: " + result);
