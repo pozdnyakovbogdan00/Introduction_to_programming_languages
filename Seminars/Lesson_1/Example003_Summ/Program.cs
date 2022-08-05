a9d31a3d1c7ea614148a11fe24b6af5c887182d0
Bogdan <pozdnyakov.bogdan00@gmail.com>
2022-08-05 07:37:19 +0600

    Make Exercise 3. Programm Summ. Folder Example003_Summ.

---
+++ Seminars/Lesson_1/Example003_Summ/Program.cs
@@ -0,0 +1,6 @@
+﻿int NumberA = new Random().Next(1, 10);
+int NumberB = new Random().Next(1, 10);
+Console.WriteLine("First number: " + NumberA);
+Console.WriteLine("Second number: " + NumberB);
+int result = NumberA + NumberB;
+Console.WriteLine("Result: " + result);
