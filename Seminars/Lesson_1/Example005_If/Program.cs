3f13d53da9637dc2b009048c193c068705423cb6
Bogdan <pozdnyakov.bogdan00@gmail.com>
2022-08-05 07:48:01 +0600

    Make Exercise 5. Programm If. Folder Example005_If.

---
+++ Seminars/Lesson_1/Example005_If/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Inter your neme: ");
+string NameUser = Console.ReadLine();
+
+if (NameUser.ToUpper() == "BOGDAN")
+{
+    Console.WriteLine("Hello Dear, we now that you special client, " + NameUser);
+}
+else
+{
+    Console.WriteLine("Hello Dear, " + NameUser);
+}
