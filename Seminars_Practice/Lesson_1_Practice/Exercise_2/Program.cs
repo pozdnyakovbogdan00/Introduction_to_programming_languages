a7c597acb90ccff78bd90859d64092eb43d1ea06
Bogdan <pozdnyakov.bogdan00@gmail.com>
2022-08-08 09:14:28 +0600

    Make exercise_2 Seminar lesson 2

---
+++ Seminars_Practice/Lesson_1_Practice/Exercise_2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Insert Number of the day: ");
+int NumberDay = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+string NameOfTheDay;
+NameOfTheDay = "";
+
+if (NumberDay > 0 & NumberDay < 8)
+{
+    if (NumberDay == 1) NameOfTheDay = "Monday";
+    if (NumberDay == 2) NameOfTheDay = "Tuesday";
+    if (NumberDay == 3) NameOfTheDay = "Wednesday";
+    if (NumberDay == 4) NameOfTheDay = "Thursday";
+    if (NumberDay == 5) NameOfTheDay = "Friday";
+    if (NumberDay == 6) NameOfTheDay = "Satrurday";
+    if (NumberDay == 7) NameOfTheDay = "Sunday"; 
+}
+else
+{
+    NameOfTheDay = "Неправильно введен номер дня недели [1-7]";   
+}
+
+Console.WriteLine($"{NameOfTheDay}");
