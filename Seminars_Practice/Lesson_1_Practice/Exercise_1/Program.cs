f60b3a080bbdda4e28bd650a760118a4ca10c91a
Bogdan <pozdnyakov.bogdan00@gmail.com>
2022-08-08 08:12:51 +0600

    Make exercise_1 Seminar lesson 1

---
+++ Seminars_Practice/Lesson_1_Practice/Exercise_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Inter variable A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("\nInter variable B: ");
+int b = int.Parse(Console.ReadLine());
+string TextOutPut;
+
+if (a == b*b)
+{
+   TextOutPut = "Первое число является квадратом второго";
+}
+else
+{
+    TextOutPut = "Первое число не является квадратом второго";
+}
+
+Console.WriteLine($"\n{TextOutPut}");
