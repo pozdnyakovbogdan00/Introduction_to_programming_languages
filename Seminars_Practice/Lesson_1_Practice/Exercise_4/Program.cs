f8881240559f0f972d6f28ba52fcfee40eae612f
Bogdan <pozdnyakov.bogdan00@gmail.com>
2022-08-08 09:42:13 +0600

    Make exercise_4 Seminar lesson 4

---
+++ Seminars_Practice/Lesson_1_Practice/Exercise_4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Insert number 1, number 2 and number 3: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+int N2 = Convert.ToInt32(Console.ReadLine());
+int N3 = Convert.ToInt32(Console.ReadLine());
+double Number_1_Final = N1%10;
+double Number_2_Final = N2%10;
+double Number_3_Final = N3%10;
+Console.WriteLine($"Result: {Number_1_Final} , {Number_2_Final} , {Number_3_Final}");
