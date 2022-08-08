fc2c6a868f039b9a811cb9001dc00fc88071650a
Bogdan <pozdnyakov.bogdan00@gmail.com>
2022-08-08 09:29:00 +0600

    Make exercise_3 Seminar lesson 3

---
+++ Seminars_Practice/Lesson_1_Practice/Exercise_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Insert N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int i = -N;
+while (i < N+1)
+{
+    if (i%2 == 0)
+    {
+        Console.WriteLine(i);        
+    }
+    i = i + 1;    
+}
